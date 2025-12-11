ae5e72f6e42657870f7b27978d8e89215d56b4a6
aymayesim <153860398+aymayesim@users.noreply.github.com>
2025-12-11 22:40:26 +0300

    Add list of Uşak addresses
    Added a list of addresses in Uşak, Turkey.

---
+++ data/addresses.py
@@ -0,0 +1,17 @@
+USAK_ADDRESSES = [
+    "Kemalöz Mahallesi, Uşak",
+    "Atapark Mahallesi, Uşak",
+    "Dikilitaş Mahallesi, Uşak",
+    "Elmalıdere Mahallesi, Uşak",
+    "Fatih Mahallesi, Uşak",
+    "Karaaltı Mahallesi, Uşak",
+    "Aybek Mahallesi, Uşak",
+    "Işık Mahallesi, Uşak",
+    "Kalfa Mahallesi, Uşak",
+    "Koyunbey Mahallesi, Uşak",
+    "Bozkurt Mahallesi, Uşak",
+    "Kurtuluş Mahallesi, Uşak",
+    "Fevzi Çakmak Mahallesi, Uşak",
+    "Atatürk Mahallesi, Uşak",
+    "Cumhuriyet Mahallesi, Uşak"
+]
