1f544f32a53394bf11e2b4400c70b8ab1f3f7b31
aymayesim <153860398+aymayesim@users.noreply.github.com>
2025-12-11 22:51:40 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+
+# Varsayılan ACO Parametreleri
+DEFAULT_ANT_COUNT = 30
+DEFAULT_ITERATIONS = 80
+DEFAULT_ALPHA = 1.0       # feromon etkisi
+DEFAULT_BETA = 2.0        # sezgisel bilgi etkisi
+DEFAULT_RHO = 0.5         # buharlaşma oranı
+DEFAULT_Q = 100           # feromon birikim katsayısı
