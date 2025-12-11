1ebff466580225ac9099b7c920667f691bd19793
aymayesim <153860398+aymayesim@users.noreply.github.com>
2025-12-11 22:37:18 +0300

    Add files via upload

---
+++ addresses.py
@@ -0,0 +1,18 @@
+
+USAK_ADDRESSES = [
+    "Kemalöz Mahallesi, Uşak",
+    "Elmalidere Mahallesi, Uşak",
+    "Kurtuluş Mahallesi, Uşak",
+    "Atatürk Mahallesi, Uşak",
+    "Aybek Mahallesi, Uşak",
+    "Karaağaç Mahallesi, Uşak",
+    "Dikilitaş Mahallesi, Uşak",
+    "Işık Mahallesi, Uşak",
+    "Fatih Mahallesi, Uşak",
+    "Fevzi Çakmak Mahallesi, Uşak",
+    "Sarayaltı Mahallesi, Uşak",
+    "Bozkurt Mahallesi, Uşak",
+    "Cumhuriyet Mahallesi, Uşak",
+    "Durak Mahallesi, Uşak",
+    "Mehmet Akif Ersoy Mahallesi, Uşak"
+]
